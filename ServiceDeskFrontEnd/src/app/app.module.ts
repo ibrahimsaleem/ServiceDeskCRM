a9e4b4d9705a19781bbdff860df13ab0b52bf5db
Mohammad Ibrahim Saleem <ibrahimsaleem9123@gmail.com>
2025-07-21 23:48:48 -0500

    Frontend Angular configuration and main module

---
+++ ServiceDeskFrontEnd/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { TicketdetailsComponent } from './ticketdetails/ticketdetails.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TicketsearchComponent } from './ticketsearch/ticketsearch.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TicketsComponent,
+    TicketdetailsComponent,
+    MessagesComponent,
+    DashboardComponent,
+    TicketsearchComponent,
+    LoginpageComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
